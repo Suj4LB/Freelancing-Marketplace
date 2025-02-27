976d9700aa88c2be7c98af7708eaeb2f6b16d559
Sujal Bhandari <116736283+whysujalwhy@users.noreply.github.com>
2025-02-27 22:20:35 +0530

    added gig model

--- api/server.js
+++ api/server.js
@@ -13,6 +13,8 @@ const connect = async () => {
     }
 }
 
+// @21:35
+
 // app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 // app.use(express.json());
 // app.use(cookieParser());
