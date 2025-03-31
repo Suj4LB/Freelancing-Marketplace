63f1248fa8ad2be4610845a7ac266695ebc396d1
Sujal Bhandari <116736283+whysujalwhy@users.noreply.github.com>
2025-03-31 20:28:47 +0530

    routing and controllers

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,25 @@
+import User from "../models/user.model.js"
+
+export const register = async (req, res) => {
+    try{
+        const newUser = new User ({
+            username:"test",
+            email:"test",
+            password:"test",
+            country:"test",
+        })
+
+        await newUser.save();
+        res.status
+    }catch(err){
+        res.status(500).send("Something went wrong!")
+    }
+}
+
+export const login = async (req, res) => {
+
+}
+
+export const logout = async (req, res) => {
+
+}
--- api/server.js
+++ api/server.js
@@ -1,5 +1,12 @@
 import mongoose from "mongoose";
 import express from "express";
+// import userRoute from "./routes/user.route.js";
+import userRoute from  "./routes/user.route.js";
+import gigRoute from "./routes/gig.route.js";
+import orderRoute from "./routes/order.route.js";
+import conversationRoute from "./routes/conversation.route.js";
+import messageRoute from "./routes/message.route.js";
+import reviewRoute from "./routes/review.route.js";
 
 const app = express();
 mongoose.set('strictQuery', true);
@@ -20,6 +27,12 @@ const connect = async () => {
 // app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 // app.use(express.json());
 // app.use(cookieParser());
+app.use("/api/users", userRoute);
+app.use("/api/gigs", gigRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/conversations", conversationRoute);
+app.use("/api/messages", messageRoute);
+app.use("/api/reviews", reviewRoute);
 
 // app.use((err, req, res, next) => {
 //   const errorStatus = err.status || 500;
