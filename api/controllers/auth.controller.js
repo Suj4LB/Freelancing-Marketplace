5bcaca6bcefa25d2d5a6a07d4b7664ed5441cc1a
Sujal Bhandari <116736283+whysujalwhy@users.noreply.github.com>
2025-04-03 22:02:18 +0530

    commit

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -10,7 +10,7 @@ export const register = async (req, res) => {
         })
 
         await newUser.save();
-        res.status
+        res.status(201).send("User has been created.")
     }catch(err){
         res.status(500).send("Something went wrong!")
     }
--- api/server.js
+++ api/server.js
@@ -1,19 +1,19 @@
 import mongoose from "mongoose";
 import express from "express";
-// import userRoute from "./routes/user.route.js";
 import userRoute from  "./routes/user.route.js";
 import gigRoute from "./routes/gig.route.js";
 import orderRoute from "./routes/order.route.js";
 import conversationRoute from "./routes/conversation.route.js";
 import messageRoute from "./routes/message.route.js";
 import reviewRoute from "./routes/review.route.js";
+import authRoute from "./routes/auth.route.js";
 
 const app = express();
 mongoose.set('strictQuery', true);
 
 const connect = async () => {
     try {
-        await mongoose.connect('mongodb+srv://projectdb:projectdb@cluster0.akcf7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+        await mongoose.connect('mongodb+srv://projectdb:projectdb@freelance.akcf7.mongodb.net/?retryWrites=true&w=majority&appName=freelance');
         console.log("Connection with mongoDB is successful! ");
     } catch (error) {
         console.log(error);
@@ -22,11 +22,12 @@ const connect = async () => {
 
 // yarn run dev
 
-// @21:35
+// @38:50
 
 // app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 // app.use(express.json());
 // app.use(cookieParser());
+app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/gigs", gigRoute);
 app.use("/api/orders", orderRoute);
